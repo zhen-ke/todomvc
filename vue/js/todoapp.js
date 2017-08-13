07593e4d9c53f855142dab78f16e9594967ec2fc
zhen-ke <cool420984@gmail.com>
2017-08-13 14:26:20 +0800

    新增Vue版并与ES6版分离

---
+++ vue/js/todoapp.js
@@ -0,0 +1,121 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    list: [],
+    val: "",
+    showing: 'all',
+    editing: -1,
+  },
+  created: function() {
+    try {
+      this.list = JSON.parse(localStorage.todo)
+    } catch(e) {
+      console.log(e)
+    }
+  },
+  methods: {
+    destroy: function(index) { // 删除条目
+      this.list.splice(index,1)
+    },
+    newtodo: function() { // 添加新条目
+      if(this.val.trim()) {
+        this.list.push({"title":this.val, checked: false})
+        this.val = ""
+      }
+    },
+    status: function(index) { // 更新条目已经完成与未完成的状态
+      if(this.list[index].checked) {
+        this.list[index].checked = true
+      }else {
+        this.list[index].checked = false
+      }
+    },
+    toggleall: function() { // 全选与反选条目
+      let check = this.list.every(it => it.checked)
+      if(check) {
+        this.list.forEach(it => it.checked = false)
+      }else {
+        this.list.forEach(it => it.checked = true)
+      }
+    },
+    changeShowing(s) { // 全选、已完成、未完成面板切换
+      this.showing = s
+    },
+    clearcompleted: function() { // 清除已经完成条目
+      this.list = this.list.filter(it => !it.checked)
+    },
+    edits: function(index, e) {
+      this.editing = index
+      setTimeout(function() {
+        e.target.parentNode.nextElementSibling.focus()
+      },0)
+    },
+    // 不使用双向绑定实现Esc撤消用户内容
+    // edittext: function(index,e) {
+    //   if(this.list[index].title === '') {
+    //     this.list.splice(index,1)
+    //   }
+    //   this.list[index].title = e.target.value
+    //   this.editing = -1
+    // }
+    edittext: function(index,e) {
+      if(this.list[index].title === '') {
+        this.list.splice(index,1)
+      }
+      this.editing = -1
+    }
+  },
+  computed: {
+    tooglestatus: function() {
+      if(this.list.length === 0) {
+        return false
+      }
+      let check = this.list.every(it => it.checked)
+      if(check) {
+        return this.checked = true
+      }else{
+        return this.checked = false
+      }
+    },
+    showbar: function() {
+      if(this.list.length !== 0) {
+        return true
+      }else {
+        return false
+      }
+    },
+    showcompleted: function() {
+      let check = this.list.some(it => it.checked)
+      if(check) {
+        return true
+      }else{
+        return false
+      }
+    },
+    showingItems(){
+      if (this.showing=='all') {
+        return this.list
+      }
+      if (this.showing=='active') {
+        return this.list.filter(it => !it.checked)
+      }
+      if (this.showing=='done') {
+        return this.list.filter(it => it.checked)
+      }
+    },
+    allCount: function() {
+      return this.list.filter(it => !it.checked).length
+    },
+    completedCount: function() {
+      return this.list.filter(it => it.checked).length
+    },
+  },
+  watch: { // 深度监控条目数据，并储存到localStorage里
+    list: {
+      handler: function (newVal, oldVal) {
+        localStorage.todo = JSON.stringify(newVal)
+      },
+      deep: true
+    }
+  }
+})
---
+++ vue/js/weather.js
@@ -0,0 +1,188 @@
+let weathertop = document.querySelector('.weathertop')
+let weathertopCity = weathertop.querySelector('.city')
+let weathertopIcon = weathertop.querySelector('.icon').querySelector('img')
+let weathertopTemperature = weathertop.querySelector('.temperature')
+let weatherapp = document.querySelector('.weatherapp')
+let userLocation = weatherapp.querySelector('.city')
+let weatherUpdateTime = weatherapp.querySelector('time')
+let todayWeather = weatherapp.querySelector('.todayweather')
+let todayWeatherIcon = todayWeather.querySelector('.icon').querySelector('img')
+let todayWeatherCurrently = todayWeather.querySelector('.temperature')
+let todayWeatherSummary = todayWeather.querySelector('.summary')
+let todayWeatherdewPoint = todayWeather.querySelector('.dewPoint')
+let todayWeatherPrecipProbability = todayWeather.querySelector('.precipProbability')
+let todayWeatherWindSpeed = todayWeather.querySelector('.windSpeed')
+let todayWeatheruvIndex = todayWeather.querySelector('.uvIndex')
+let weatherError = document.querySelector('.error')
+
+// 天气错误代码
+let errorMap = {
+  "1": "位置服务被拒绝",
+  "2": "暂时获取不到位置信息",
+  "3": "获取信息超时",
+  "4": "未知错误",
+  "5": "您的浏览器不支持地理位置定位"
+}
+
+// 通过 JSONP 请求数据
+function jsonp(url) {
+  var script = document.createElement('script');
+  script.setAttribute('src', url);
+  document.body.appendChild(script);
+  return new Promise(function(resolve, reject) {
+    script.onload = function() {
+      resolve(script)
+    }
+    script.onerror = function() {
+      reject()
+    }
+  })
+}
+
+// 处理 JSONP 请求数据的回调
+function callback(data) {
+  refreshWeather(data)
+  moreDays(data)
+}
+
+// 刷新天气
+function refreshWeather(data) {
+  weatherUpdateTime.innerHTML = getTime(data.currently.time).replace(/[^0-9]*/, "") + " Update"
+  weathertopIcon.src = todayWeatherIcon.src = iconMap[(data.currently.icon).split('-').join("")]
+  weathertopTemperature.innerHTML = todayWeatherCurrently.innerHTML = Math.ceil(data.currently.temperature) + "˚"
+  todayWeatherSummary.innerHTML = data.currently.summary
+  todayWeatherdewPoint.innerHTML = parseInt(data.currently.dewPoint) + "%"
+  todayWeatherPrecipProbability.innerHTML = parseInt(data.currently.precipProbability * 100) + "%"
+  todayWeatheruvIndex.innerHTML = data.currently.uvIndex
+}
+
+// 使用 IP 请求用户地理位置
+// 如果失败则通过 navigator.geolocation 获取用户地理位置
+jsonp("https://freegeoip.net/json/?callback=loc").then(function(script) {
+  document.body.removeChild(script)
+}).catch(function() {
+  getLocation()
+})
+
+
+
+// 如果获取到 IP ，则开始生成天气
+function loc(str) {
+  getWeather(str)
+}
+
+// 如果获取到经纬度，则开始生成天气
+function onSuccess(position) {
+  getWeather(position.coords)
+}
+
+// 通过 navigator.geolocation 获取用户地理位置
+function getLocation() {
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(onSuccess, onError); // 地理位置服务可用
+  } else {
+    werror(5) // 地理位置服务不可用
+  }
+}
+
+// 处理用户地理位置错误时状态
+function onError(error) {
+  switch (error.code) {
+    case 1:
+      werror(error.code)
+      break;
+    case 2:
+      werror(error.code)
+      break;
+    case 3:
+      werror(error.code)
+      break;
+    case 4:
+      werror(error.code)
+      break;
+  }
+}
+
+// 天气错误提示
+function werror(err) {
+  let weatherError = document.createElement('p')
+  weatherError.innerHTML = errorMap[err]
+  weatherError.setAttribute('class', 'error')
+  document.body.appendChild(weatherError)
+  setTimeout(function() {
+    document.body.removeChild(weatherError)
+  }, 2000)
+}
+
+// 请求用户天气数据
+function getWeather(str) {
+  let latitude = parseInt(str.latitude)
+  let longitude = parseInt(str.longitude)
+  for (let key in data) {
+    let temp = data[key].split(",")
+    let latitudeTemp = parseInt(data[key].split(",")[0])
+    let longitudeTemp = parseInt(data[key].split(",")[1])
+    if (latitudeTemp === latitude && longitudeTemp === longitude) {
+      jsonp('https://api.darksky.net/forecast/b534fc093637c2e5fccdbe93f777fcda/' + data[key] + '?units=si&lang=zh' + '&callback=callback').then(function(script) {
+        weathertop.style.display = 'block'
+        document.body.removeChild(script)
+      })
+      userLocation.innerHTML = weathertopCity.innerHTML = key
+      break;
+    }
+  }
+}
+
+// 刷新未来天气
+function moreDays(str) {
+  let weatherWbody = weatherapp.querySelector('.wbody')
+  let weatherWbodyDl = weatherapp.querySelectorAll('dl')
+  for (let key in weatherWbodyDl) {
+    if (key === 'length') break
+    let weatherWbodyWeek = weatherWbodyDl[key].querySelector('dt')
+    let weatherWbodyIcon = weatherWbodyDl[key].querySelector('.icon').querySelector('img')
+    let weatherWbodySummary = weatherWbodyDl[key].querySelector('.summary')
+    let weatherWbodyTemperatureMin = weatherWbodyDl[key].querySelector('.temperaturemin')
+    let weatherWbodyTemperatureMax = weatherWbodyDl[key].querySelector('.temperaturemax')
+    let weatherPrecipProbability = weatherWbodyDl[key].querySelector('.summary')
+    weatherPrecipProbability.innerHTML = parseInt(str.daily.data[key].precipProbability * 100) + "%"
+    weatherWbodyIcon.src = iconMap[(str.daily.data[key].icon).split('-').join('')]
+    weatherWbodyTemperatureMin.innerHTML = Math.ceil(str.daily.data[key].temperatureMin) + "˚"
+    weatherWbodyTemperatureMax.innerHTML = Math.ceil(str.daily.data[key].temperatureMax) + "˚"
+    // weatherWbodySummary.innerHTML = str.daily.data[key].summary
+    if (+key === 0) {
+      continue
+    } else {
+      weatherWbodyWeek.innerHTML = getDay(str.daily.data[key].time)
+    }
+  }
+}
+
+// 秒转星期
+function getDay(time) {
+  let arr = ["周日", "周一", "周二", "周三", "周四", "周五", "周六"];
+  return arr[new Date(new Date(parseInt(time) * 1000).toLocaleString().split(" ")[0]).getDay()]
+}
+
+// 秒转时间
+function getTime(time) {
+  return new Date(parseInt(time) * 1000).toLocaleString().split(" ")[1]
+}
+
+// let search = document.querySelector('input')
+// search.addEventListener('input', debounce(function() {
+//   if (search.value.trim()) {
+//     jsonp('https://api.darksky.net/forecast/b534fc093637c2e5fccdbe93f777fcda/' + data[search.value] + '?units=si&lang=zh' + '&callback=callback')
+//   }
+//   userLocation = search.value
+// }, 500))
+
+// function debounce(fn, duration) {
+//   var timerId
+//   return function(...args) {
+//     clearTimeout(timerId)
+//     timerId = setTimeout(function() {
+//       fn(...args)
+//     }, duration)
+//   }
+// }
